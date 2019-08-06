2e1e0a128910cb641fdc4fedb0697f4196a1c21d
unknown <you@example.com>
2019-08-06 12:41:43 +0430

    a little change in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.red,
         accentColor: Colors.redAccent,
       ),
-      home: ItemInfo(),
+      home: HomePage(),
     );
   }
 }
